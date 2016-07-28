d3375bfe7c8f80e4c5eb35914e04b4cf56a27333
cihannisanci <cihannisan@gmail.com>
2016-07-27 21:44:16 -0700

    Finish functional classes with methods and prototypal classes

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.queueSize = 0;
+  newQueue.count = 0;
+  newQueue.storage = {};
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function () {
+    return this.queueSize;
+  },
+
+  enqueue: function (value) {
+    this.storage[this.queueSize + this.count] = value;
+    this.queueSize++;
+    return this.storage;
+  },
+
+  dequeue: function () {
+    if (this.queueSize === 0) {
+      return this.queueSize = 0;
+    }
+    this.queueSize--;
+    var obj = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count++;
+    return obj;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.stackSize = 0;
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  size: function() {
+    return this.stackSize;
+  },
+  push: function(value) {
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+    return this.storage;
+  },
+  pop: function() {
+    if (this.stackSize === 0) {
+      return 0;
+    }
+    this.stackSize--;
+    var obj = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return obj;
+  }
+};
 
 
