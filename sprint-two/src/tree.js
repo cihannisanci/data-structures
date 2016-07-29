7d8d74db5b134670adc1bc763cd0c90fba1927c7
cihannisanci <cihannisan@gmail.com>
2016-07-29 12:10:18 -0700

    Adds children to tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,18 +1,22 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  // your code here  
+
+  var anotherTree = Tree(value);
+  this.children.push(anotherTree);
+
+
+  console.log(Tree(value));  // fix me
 };
 
 treeMethods.contains = function(target) {
