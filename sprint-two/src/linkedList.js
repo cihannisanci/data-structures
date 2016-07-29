19003324f31351a314ce96ad72b202e7b291f0ed
cihannisanci <cihannisan@gmail.com>
2016-07-28 21:09:55 -0700

    Passes linked lists tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,36 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    list.tail = newNode;
+    if (list.head === null) {
+      list.head = list.tail;
+    } else {
+      list.head.next = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var obj = list.head;
+    list.head = list.head.next;
+    // if (list.contains) {
+    //   return obj === list.head;
+    // }
+    return obj.value;
   };
 
   list.contains = function(target) {
+    var loc = list.head;
+    while (loc) {
+      if (loc.value === target) {
+        return true;
+      }
+      loc = loc.next;
+    }
+    return false;
   };
 
   return list;
@@ -23,7 +45,3 @@ var Node = function(value) {
 
   return node;
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
