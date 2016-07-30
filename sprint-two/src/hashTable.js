ce14205ea4d2695bde65cb66825f854ded239c2b
cihannisanci <cihannisan@gmail.com>
2016-07-30 00:07:27 -0700

    Adds comments for hash table collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,19 +3,36 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  console.log(this._storage);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) !== undefined) {
+    this._storage.set(index, this._storage.set(this._count, k));
+    this._count++;
+    // nestedArr[k] = v;
+  }
+  this._storage.set(index, v);
+  // console.log(this._storage, 'insert');
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // if (this._storage.get(index).length !== this._count) {
+  //   return 'yes';
+  // }
+  
+  // if (this._storage.get(index).length) {
+  //   return 'yes';
+  // }
+  return this._storage.get(index);
+  // console.log(this._storage, 'retrieve');
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
