04433ccfb3b5295e5f30e1c4b0972b115cea4078
cihannisanci <cihannisan@gmail.com>
2016-07-30 18:48:52 -0700

    Starts BST implementation

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,44 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function() {
+  var newTree = {};
+
+  newTree.insert = function (value) {
+    var newLeaf = new Leaf(value);
+    
+    if (newLeaf.value < value) {
+      console.log(newLeaf.value);
+      newLeaf.left = value;
+      console.log(newLeaf.value);
+    } else if (newLeaf.value > value) {
+      console.log(newLeaf.value);
+      newLeaf.right = value;
+      console.log(newLeaf.value);
+    } else {
+      console.log(newLeaf.value);
+      console.log(newLeaf.right);
+      return undefined;
+    }
+  };
+
+  newTree.contains = function (value) {
+
+  };
+
+  newTree.depthFirstLog = function (value) {
+
+  };
+
+  return newTree;
 };
 
+var Leaf = function (value) {
+  var leaf = {};
+
+  leaf.value = value;
+  leaf.left = null;
+  leaf.right = null;
+
+  return leaf;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
